ff364389a83868c86aa9ef414e86d46e8aac186a
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2021-09-17 11:50:19 +0200

    mapje met printscreens voor de python programma's

--- Feestlunch.py
+++ Feestlunch.py
@@ -2,4 +2,7 @@ croissant = 17
 stokbrood = 2
 kortingbonnen = 3
 
-print((croissant * 0.39) + (stokbrood * 2.78) - (kortingbonnen * 0.50))
+saldo = (croissant * 0.39) + (stokbrood * 2.78) - (kortingbonnen * 0.50)
+
+
+print('De feestlunch kost je bij de bakker ' + str(saldo) + ' euro voor de ' + str(croissant) + ' croissantjes en de ' + str(stokbrood) + ' stokbroden als de ' + str(kortingbonnen) + ' kortingsbonnen nog geldig zijn!')
--- Speelhaldag.py
+++ Speelhaldag.py
@@ -2,5 +2,8 @@ personen = 3
 toegangticket = 7.45
 bedragper5minuten = 0.37
 aantalkeer = 9
+aantalminuten = 45
 
-print(personen * (toegangticket + bedragper5minuten * aantalkeer))
+saldo = personen * (toegangticket + bedragper5minuten * aantalkeer)
+
+print('Dit geweldige dagje-uit met ' + str(personen) + ' mensen in de Speelhal met ' + str(aantalminuten) + ' minuten VR kost je maar ' + str(saldo) + ' euro')
